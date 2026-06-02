430205ed19d98d3c02e26437306e798f3abb1f42
Gokul Muthuramalingam <144006770+gokulshiva77@users.noreply.github.com>
2026-06-02 11:30:12 +0000

    Enable user input for Class of Device in HEX

--- main.cpp
+++ main.cpp
@@ -3,11 +3,9 @@
 
 int main(int argc, char** argv){
     uint32_t cod_in;
-    // std::cout << "Enter Class of device in HEX 0x: ";
-    // std::cin >> std::hex >> cod_in;
-    //  std::cout << "You entered: 0x" << std::hex << cod_in << std::endl;
-    cod_in = 0x6A0110;
-    // cod_in = 0x5A020C;
+    std::cout << "Enter Class of device in HEX 0x: ";
+    std::cin >> std::hex >> cod_in;
+    std::cout << "You entered: 0x" << std::hex << cod_in << std::endl;
     ClassOfDevice cod(cod_in);
     cod.PrintClassOfDevice();
    
